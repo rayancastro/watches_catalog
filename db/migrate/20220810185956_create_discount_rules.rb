7963219dccd5f708846f3babf16cdcf4d9e33591
Rayan Castro <rayancdc@gmail.com>
2022-08-18 14:03:22 +0200

    Fix migration

--- db/migrate/20220810185956_create_discount_rules.rb
+++ db/migrate/20220810185956_create_discount_rules.rb
@@ -2,8 +2,8 @@ class CreateDiscountRules < ActiveRecord::Migration[7.0]
   def change
     create_table :discount_rules do |t|
       t.references :watch, null: false, foreign_key: true
-      t.integer :bundle_size
-      t.integer :bundle_price_cents
+      t.integer :discount_quantity
+      t.integer :discounted_price_cents
 
       t.timestamps
     end
--- db/migrate/20220811110151_refactor_names_from_discount_rules.rb
+++ db/migrate/20220811110151_refactor_names_from_discount_rules.rb
@@ -1,6 +1,6 @@
 class RefactorNamesFromDiscountRules < ActiveRecord::Migration[7.0]
   def change
-    rename_column :discount_rules, :bundle_size, :bundle_size
-    rename_column :discount_rules, :bundle_price_cents, :bundle_price_cents
+    rename_column :discount_rules, :discount_quantity, :bundle_size
+    rename_column :discount_rules, :discounted_price_cents, :bundle_price_cents
   end
 end
